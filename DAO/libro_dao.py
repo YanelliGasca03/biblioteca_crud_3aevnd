e99a258cfa5e1ed3c2cf560982ac3e96e8a11e96
YanelliGasca03 <yanelligasca@gmail.com>
2026-06-23 12:43:01 -0600

    Usando consultas almacenas o vistas

--- DAO/libro_dao.py
+++ DAO/libro_dao.py
@@ -10,19 +10,9 @@ class LibroDAO:
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
 
-        sql = '''
-            SELECT 
-                libro.id_libro,
-                libro.titulo,
-                autor.nombre as autor,
-                libro.isbn,
-                libro.disponible
-            FROM libro
-            INNER JOIN autor
-            ON libro.autor = autor.id 
-            '''
-
-        cursor.execute(sql)
+        
+# Ejecuta la consulta almacenada (vista)
+        cursor.execute("SELECT * FROM vista_libros")
         registros = cursor.fetchall()
 
         libros = []
